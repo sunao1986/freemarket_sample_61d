2dd4256d87d7764d63746595ff937ea32900a5b3
sunao1986 <nk@sunao-products.com>
2019-10-27 13:30:00 +0900

    basic_set

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,24 @@
+class ItemsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def updata
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def show
+  end  
+
+end
