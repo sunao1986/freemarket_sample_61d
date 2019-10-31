e2652a80a579acef613f0973c5a50b7b020e76cd
tky106 <tktktky106@gmail.com>
2019-10-31 14:12:34 +0900

    remove a file

--- db/schema.rb
+++ db/schema.rb
@@ -59,7 +59,6 @@ ActiveRecord::Schema.define(version: 20191030080136) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",                                null: false
     t.string   "email",                                default: "", null: false
     t.string   "encrypted_password",                   default: "", null: false
     t.string   "reset_password_token"
@@ -80,8 +79,8 @@ ActiveRecord::Schema.define(version: 20191030080136) do
     t.string   "address"
     t.string   "building"
     t.string   "nickname"
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["name"], name: "index_users_on_name", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
