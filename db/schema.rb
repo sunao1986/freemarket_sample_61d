eb97d483a9a6f9a66d32e6228d9f384d3104fea7
tky106 <tktktky106@gmail.com>
2019-11-01 11:31:08 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20191030080136) do
     t.string   "address"
     t.string   "building"
     t.string   "nickname"
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
