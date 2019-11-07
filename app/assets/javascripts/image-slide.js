25ee53158a43ebbae43132df76efa0454f9599bf
hashimotoharuka <hashimoto.inui0306@gmail.com>
2019-11-07 13:11:03 +0900

    slick修正

--- app/assets/javascripts/image-slide.js
+++ app/assets/javascripts/image-slide.js
@@ -1,9 +1,9 @@
 $(document).on('turbolinks:load', function(){
   $('.item-container__item-main__photo__act').slick({
     asNavFor:'.item-container__item-main__photo__inact',
-    infinite: false,
-    draggable: false
+    draggable: false,
   });
+  
   $('.item-container__item-main__photo__inact').slick({
     asNavFor:'.item-container__item-main__photo__act',
     focusOnSelect: true,
