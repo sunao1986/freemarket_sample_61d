0af7a0029242e3a02e1bea9a4eaa5d34073742c0
sunao1986 <nk@sunao-products.com>
2019-11-08 19:20:35 +0900

    schma_updata

--- db/schema.rb
+++ db/schema.rb
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20191107085823) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",                                              null: false
     t.string   "email",                                default: "", null: false
     t.string   "encrypted_password",                   default: "", null: false
     t.string   "reset_password_token"
@@ -111,11 +112,11 @@ ActiveRecord::Schema.define(version: 20191107085823) do
     t.string   "address"
     t.string   "building"
     t.string   "nickname"
-    t.string   "name"
     t.string   "provider"
     t.string   "uid"
     t.string   "image"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["name"], name: "index_users_on_name", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
