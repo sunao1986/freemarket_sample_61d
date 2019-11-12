31b7dae0bcaa48d7ff91b42d520e9fa8fd7b3818
chanoku0201 <okudakazunori@okudakazunorinoMacBook-Air.local>
2019-11-12 14:35:11 +0900

    パンくず

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -42,39 +42,10 @@ crumb :logout do
   link "ログアウト", logout_users_path
   parent :mypage
 end
-# # -------------------------------------------
-
-# # カテゴリー-------------------------------------------------
-
-# crumb :root_category do
-#   link "カテゴリー一覧", categories_path
-#   parent :root
-# end
-
-# crumb :child_category do
-#   link "#{}", categories_path()
-#   parent :root_category
-# end
 
 
 
 
-
-
-# crumb :project do |project|
-#   link project.name, project_path(project)
-#   parent :projects[]
-# end
-
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
-
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
 
 # If you want to split your breadcrumbs configuration over multiple files, you
 # can create a folder named `config/breadcrumbs` and put your configuration
