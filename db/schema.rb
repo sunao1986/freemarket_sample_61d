18401f184a9923677bdd226edfdbf9e94cdfc65f
tky106 <tktktky106@gmail.com>
2019-11-16 21:04:20 +0900

    generate a migration file and execute rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191115111053) do
+ActiveRecord::Schema.define(version: 20191116112925) do
 
   create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -122,6 +122,8 @@ ActiveRecord::Schema.define(version: 20191115111053) do
     t.string   "provider"
     t.string   "uid"
     t.string   "image"
+    t.string   "birthyear"
+    t.string   "birthmonth"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
