cad0523a7faf41c1a2ab54f28f81b99322617200
tky106 <tktktky106@gmail.com>
2019-11-18 12:32:58 +0900

    remove userless commentouts

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -39,31 +39,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to signup_registration_path
     end
   end
-  # def facebook
-  #   # You need to implement the method below in your model (e.g. app/models/user.rb)
-  #   @user = User.from_omniauth(request.env["omniauth.auth"])
-
-  #   if @user.persisted?
-  #     sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-  #     set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-  #   else
-  #     session["devise.facebook_data"] = request.env["omniauth.auth"]
-  #     redirect_to new_user_registration_url
-  #   end
-  # end
-
-  # def google_oauth2
-  #   # You need to implement the method below in your model (e.g. app/models/user.rb)
-  #   @user = User.from_omniauth(request.env["omniauth.auth"])
-
-  #   if @user.persisted?
-  #     sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-  #     set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
-  #   else
-  #     session["devise.google_data"] = request.env["omniauth.auth"]
-  #     redirect_to new_user_registration_url
-  #   end
-  # end
 
   def failure
     redirect_to root_path
@@ -75,4 +50,5 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def after_omniauth_failure_path_for(scope)
     super(scope)
   end
+  
 end
